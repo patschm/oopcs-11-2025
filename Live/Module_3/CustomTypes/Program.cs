cb9761bc6d09b07fe59f927fb7c187e6831cff27
Patrick Schmidt <pschmidt@pasit.nl>
2025-11-12 13:22:16 +0100

    day1

---
+++ Live/Module_3/CustomTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CustomTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = 6;
+           // WeekDays day = WeekDays.Wednesday;
+           WeekDays day = (WeekDays)dayOfWeek;
+            System.Console.WriteLine($"Enum day of week: {day} ({(int)day})");
+        }
+    }
+
+    // Custom type representing days of the week
+    enum WeekDays : ulong
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 7,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32
+    }
+}
